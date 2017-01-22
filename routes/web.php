08440714ea7f2b8fb53035bad05131819d0d1797
simon <simon@realweb.ru>
2017-01-22 23:39:08 +0300

    auth controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 Route::get('/', function () {return view('welcome');});
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('login', 'Auth\LoginController@getLogin');
+Route::post('login', 'Auth\LoginController@postLogin');
+Route::get('logout', 'Auth\LoginController@getLogout');
