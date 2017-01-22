2c048d39c32baee021001bdf2622d66ee7246ed3
simon <simon@realweb.ru>
2017-01-23 01:13:02 +0300

    login route name

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 Route::get('/', function () {return view('welcome');});
 
-Route::get('login', 'Auth\LoginController@showLoginForm');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout');
